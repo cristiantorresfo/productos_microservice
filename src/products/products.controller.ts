9d1b1cc57154b0edd799eef62c59593dd591532d
Cristian Torres <cristorresf@outlook.com>
2025-12-04 21:20:42 -0500

    feat: add microservice to get products

---
+++ src/products/products.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { ProductsService } from './products.service';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Get()
+  getAll() {
+    return this.productsService.findAll();
+  }
+
+  @Get('search')
+  search(@Query('q') query: string) {
+    return this.productsService.findByQuery(query);
+  }
+}
